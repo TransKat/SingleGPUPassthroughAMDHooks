415281f535f48d23987d97e8aaf69efb06c0031f
Kat <61893437+TransKat@users.noreply.github.com>
2022-01-12 21:00:55 -0600

    Add files via upload

---
+++ revert.sh
@@ -0,0 +1,32 @@
+
+#!/bin/bash
+set -x
+  
+# Re-Bind GPU to Nvidia Driver
+#virsh nodedev-reattach pci_0000_0b_00_0
+#virsh nodedev-reattach pci_0000_0c_00_0
+
+# Rebind VT consoles
+echo 1 > /sys/class/vtconsole/vtcon0/bind
+echo 1 > /sys/class/vtconsole/vtcon1/bind
+ 
+# Unload all the vfio modules
+modprobe -r vfio_pci
+modprobe -r vfio_iommu_type1
+modprobe -r vfio
+ 
+# Reattach the gpu
+virsh nodedev-reattach $VIRSH_GPU_VIDEO
+virsh nodedev-reattach $VIRSH_GPU_AUDIO
+ 
+# Load all Radeon drivers
+modprobe  amdgpu
+modprobe  gpu_sched
+modprobe  ttm
+modprobe  drm_kms_helper
+modprobe  i2c_algo_bit
+modprobe  drm
+modprobe  snd_hda_intel
+echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
+
+systemctl start sddm.service
---
+++ start.sh
@@ -0,0 +1,31 @@
+# debugging
+set -x
+
+# load variables we defined
+source "/etc/libvirt/hooks/kvm.conf"
+
+# Stop display manager
+systemctl stop sddm.service
+
+# Unbind VTConsoles
+echo 0 > /sys/class/vtconsole/vtcon0/bind
+echo 0 > /sys/class/vtconsole/vtcon1/bind
+
+# Unbind EFI-Framebuffer
+#echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+
+
+sleep 3
+
+
+# Unload all Radeon drivers
+modprobe -r amdgpu
+ 
+# Unbind the GPU from display driver
+virsh nodedev-detach $VIRSH_GPU_VIDEO
+virsh nodedev-detach $VIRSH_GPU_AUDIO
+ 
+# Load VFIO kernel module
+modprobe vfio
+modprobe vfio_pci
+modprobe vfio_iommu_type1
